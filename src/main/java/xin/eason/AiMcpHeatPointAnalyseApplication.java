977898dc9af0abd788f43a43b763d98e186335d1
Eason <3220563569@qq.com>
2025-05-27 11:31:24 +0800

    项目初始化

---
+++ src/main/java/xin/eason/AiMcpHeatPointAnalyseApplication.java
@@ -0,0 +1,13 @@
+package xin.eason;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiMcpHeatPointAnalyseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiMcpHeatPointAnalyseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/xin/eason/AiMcpHeatPointAnalyseApplicationTests.java
@@ -0,0 +1,13 @@
+package xin.eason;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiMcpHeatPointAnalyseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
